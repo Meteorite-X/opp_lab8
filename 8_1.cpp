44589acee01b8e50a66a0f40f6e3b7aa7dca36ff
ganboldsarangoo.github.io <ganboldsarangoo55@gmail.com>
2025-11-03 22:18:46 +0800

    Update 8_1.cpp

--- 8_1.cpp
+++ 8_1.cpp
@@ -1,50 +1,92 @@
-#include<iostream> 
-#include<cstring> 
+#include <iostream>
+#include <cmath>
+#include <cstring>
 using namespace std;
 
-class Shape{
-    protected:
-        char * name;
-        static int count; //shape class-s үүсэн объектуудыг тоолно.
-    public:
-        Shape(){
-            name = new char[20];
-            strcpy(name, "No name");
-            count++; //объект үүсэх бүрт нэмэгдэнэ.
-        }
+class Shape {
+protected:
+    char *name;
+    static int count;
+public:
+    Shape(const char *n = "No name") {
+        this->name = new char[strlen(n) + 1];
+        strcpy(this->name, n);
+        count++;
+    }
 
-        Shape(const char * n){
-            name = new char[strlen(n)+1];
-            strcpy(name, n);
-            count++; //объект үүсэх бүрт нэмэгдэнэ.
-        }
+    virtual ~Shape() {
+        delete [] this->name;
+        count--;
+    }
 
-        virtual ~Shape(){
-            delete [] name;
-            count--; //объект устах бүрт нэгээр багасна.
-        }
+    static void set_count(int c) { Shape::count = c; }
+    static int get_count() { return Shape::count; }
 
-        // count буюу хэдэн объект байгааг авах static гишүүн функц
-        static int get_count() {
-            return count;
-        }
+    char* get_name() { return this->name; }
 
-        // count-д утга оноох static г/ф
-        static void set_count(int n){
-            count = n;
-        }
+    virtual float area() const = 0;
+    virtual float perimeter() const = 0;
 };
 
-//анхны утга өгнө.
-int Shape::count = 0; 
+int Shape::count = 0;
 
+class TwoDShape : public Shape {
+protected:
+    float *x, *y;
+    int point_count;
+public:
+    TwoDShape(int n = 1, const char *nname = "2DShape") : Shape(nname) {
+        this->point_count = n;
+        this->x = new float[this->point_count];
+        this->y = new float[this->point_count];
+        for (int i = 0; i < this->point_count; i++) {
+            this->x[i] = 0;
+            this->y[i] = 0;
+        }
+    }
 
+    virtual ~TwoDShape() {
+        delete [] this->x;
+        delete [] this->y;
+    }
 
+    // Жинхэнэ хийсвэр функцууд
+    virtual float area() const = 0;
+    virtual float perimeter() const = 0;
 
-int main(){
+    void show_info() {
+        cout << "2DShape: " << this->get_name() << endl;
+    }
+};
 
-    Shape::set_count(5);
-    cout << "Shape-s uussen object: " << Shape::get_count() << endl;
+class Circle : public TwoDShape {
+    float radius;
+public:
+    Circle(float r = 1) : TwoDShape(1, "Circle") {
+        this->radius = r;
+    }
+
+    float area() const override { return M_PI * this->radius * this->radius; }
+    float perimeter() const override { return 2 * M_PI * this->radius; }
+
+    void show_info() {
+        cout << "Circle name: " << this->get_name()
+             << " | radius: " << this->radius
+             << " | area: " << this->area()
+             << " | perimeter: " << this->perimeter() << endl;
+    }
+};
+
+int main() {
+    Circle c1(3.5);
+    Circle c2(5);
+
+    cout << "Object count: " << Shape::get_count() << endl;
+
+    c1.show_info();
+    c2.show_info();
+
+    cout << "After objects destroyed, count will decrease automatically.\n";
 
     return 0;
 }
